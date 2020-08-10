741a6aabb208fec4dd3da6c03605c37a7919685c
Visenna88 <nadezhda.sirbu@gmail.com>
2020-08-10 23:52:49 +0300

    Add Material UI

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 const Cards = (props) => {
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+        
+        </div> 
     )
 }
 
